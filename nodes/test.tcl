2960569de7f97e8c5d10093e7da6436cec38ed07
Farid2001-hub <64692088+Farid2001-hub@users.noreply.github.com>
2023-04-13 14:31:17 +0200

    Update test.tcl

--- nodes/test.tcl
+++ nodes/test.tcl
@@ -1,22 +1,16 @@
-#!/usr/bin/env tclsh
+# test.tcl
+package require tcltest
+source "nodes/nouveauPc.tcl"
 
-# Charger le module du noeud PC
-package require Pc
-
-# Charger le module de simulation de réseau NS-2
-package require ns
-
-# Charger le fichier de définition de la fonction nouveauPc
-source nodes/nouveauPc.tcl
-
-# Créer un noeud PC
-set pc1 [nouveauPc "pc1"]
-
-# Tester si le noeud PC a été créé avec succès
-if {[info exists pc1] && $pc1 != ""} {
-    puts "Test succeeded: nouveauPc"
-    exit 0
-} else {
-    puts "Test failed: nouveauPc"
-    exit 1
+# Test case
+proc test_nouveauPc {} {
+    set node [nouveauPc "test_node" "192.168.1.1" "00:11:22:33:44:55" "255.255.255.0"]
+    set expected_node_info "test_node 192.168.1.1 00:11:22:33:44:55 255.255.255.0"
+    # Assert that the node info matches the expected result
+    set msg "Expected: $expected_node_info, but got: $node"
+    return [list $msg [string equal $node $expected_node_info]]
 }
+
+# Run the test
+tcltest::runSuite
+
